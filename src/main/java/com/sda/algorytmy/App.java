c36d864bbf18b63097acb6f491df61c25b7e77dc
Mateusz Erdman/Matek <mateusz.erdman@hotmail.com>
2019-05-18 12:49:48 +0100

    Sito Eratostenesa okrojona ver

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,29 @@
 package com.sda.algorytmy;
-
+import java.util.Scanner;
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj przedział liczb ");
+        int n = scanner.nextInt();
+        while(n<200) {
+            int licznik = 0;
+
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    //System.out.println("jest dzielnik: " + i);
+                    licznik++;
+                }
+            }
+            if (licznik == 0) {
+                System.out.println(n + " jest liczba pierwsza");
+            } else {
+                //System.out.println(n + " nie jest liczba pierwsza");
+            }
+         n++;
+        }
     }
 }
